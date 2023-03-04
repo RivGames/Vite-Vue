26f7c2713bf67d10a99c2641e41ea442972a8fee
Ihor Rud <Igorryd7@gmail.com>
2023-03-04 20:45:10 +0200

    composables

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -10,28 +10,14 @@ setTimeout(() => {
   message.value = 'Hi!';
 }, 2500)
 */
-import {ref,watch} from "vue";
 import {useStorage} from "@/composables/useStorage";
-let food = useStorage('food');
-// let food = ref(localStorage.getItem('food'));
-// let age = ref(localStorage.getItem('age'));
-// watch(food,() => {
-//   write('food',food.value)
-// })
-// watch(age,() => {
-//   write('age',age.value)
-// })
-// function write(key,val){
-//   localStorage.setItem(key,val)
-// }
-// setTimeout(()=>{
-//   food.value = 'changed!';
-// },2000);
+let food = useStorage('food','borsch');
+let age = useStorage('age',18);
 </script>
 
 <template>
   <main>
-    <p>What`s your favourite food? <input type="text" v-model="food"></p>
-    <p>How old are you? <input type="text" v-model="age"></p>
+    <p>What`s your favourite food ? <input type="text" v-model="food"></p>
+    <p>What`s your age ? <input type="text" v-model="age"></p>
   </main>
 </template>
